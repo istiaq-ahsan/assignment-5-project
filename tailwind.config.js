c7c485fef57bd8e88c82ac5188e3096fcd642c79
istiaq-ahsan <ahsanistiaq23@gmail.com>
2024-09-23 01:13:03 +0600

    Almost done design of project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                'header-bg': '#F9F7F3',
+                'primary-bg': '#B4F461',
+            }
+        },
+    },
+    plugins: [],
+}
